2ba09ab6036ae9dc42fca410ba995d6ada3f910a
RRoberts225 <83940047+RRoberts225@users.noreply.github.com>
2021-05-17 16:38:47 +1000

    Update MainWindow.xaml.cs
    updated namespace to Assessment2

--- Assessment2/Assessment2/View/MainWindow.xaml.cs
+++ Assessment2/Assessment2/View/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using KIT206_Week9;
 
-namespace KIT206_Week10_Sample
+namespace Assessment2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

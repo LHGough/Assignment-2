8ee3a5f34dca43e5b3c7b851c28c09a84f086cc0
RRoberts225 <83940047+RRoberts225@users.noreply.github.com>
2021-05-17 15:56:52 +1000

    Add files via upload
    Assessment 2 group 24 for KIT206
    Re-uploaded with complete references

---
+++ Assessment2/Assessment2/Model/Publication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_Week9
+{
+	//Types of publication
+	public enum Mode { Conference, Journal, Other }; //not actualy sure if these need to be changed
+
+    /// <summary>
+    /// A publication created by a researcher on their topic of research.
+    /// </summary>
+    public class Publication //change everything to publication
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+		public Mode Mode { get; set; }
+        public DateTime Certified { get; set; } //this is something different
+
+        public int Freshness
+        {
+            //DateTime.Today returns today's date. As DateTime objects overload the
+            //addition and subtraction operators we can use them to determine the
+            //elapsed time between today's date and the Completed date. However, 
+            //the result is not a number but a TimeSpan object, whose Days
+            //property gives the number of whole days represented by the TimeSpan.
+            get { return (DateTime.Today - Certified).Days; }
+        }
+
+        public override string ToString()
+        {
+            //This is a straightforward way of constructing the string using DateTime's
+            //ToShortDateString method to remove the time component of the complted date
+            return Title + " completed by " + Mode + " on " + Certified.ToShortDateString();
+            
+            //This alternative approach uses the Format method of string, with the
+            //short date format requested via the :d in the format string
+            //return string.Format("{0} completed by {1} on {2:d}", Title, Mode, Certified);
+        }
+    }
+}
+

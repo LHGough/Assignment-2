2ccaa0b062115ca5c2ad6ed1de0169991d90d9ce
RRoberts225 <83940047+RRoberts225@users.noreply.github.com>
2021-05-23 19:24:13 +1000

    Add files via upload
    reuploaded files of the renamed versions of 'boss.cs', 'agency.cs', and 'employee.cs'

---
+++ Faculty.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_A2      //used to be KIT206_week9
+{
+    class Faculty
+    {
+        //The example shown here follows the pattern discussed in the Module 6 summary slides,
+        //maintaining two collections, a master and a 'viewable' one (which is the 'view model'
+        //in Microsoft's Model-View-ViewModel approach to Model-View-Controller)
+        private List<Researcher> staff;
+        public List<Researcher> Workers { get { return staff; } set { } }
+       
+        private ObservableCollection<Researcher> viewableStaff;
+        public ObservableCollection<Researcher> VisibleWorkers { get { return viewableStaff; } set { } }
+
+        public Faculty()
+        {
+            staff = researcherDatabase.LoadAll();
+            viewableStaff = new ObservableCollection<Researcher>(staff); //this list we will modify later
+
+            //Part of step 2.3.2 from Week 8 tutorial
+            foreach (Researcher e in staff)
+            {
+                e.Skills = researcherDatabase.LoadPublications(e.ID);
+            }
+        }
+
+        public ObservableCollection<Researcher> GetViewableList()
+        {
+            return VisibleWorkers;
+        }
+
+        //The below method was in wk 8 but not wk 10 tutorial, which had the above method - is the one below still needed?
+        /// <summary>
+        /// Displays the list of employees on the console.
+        /// </summary>
+        public void Display()
+        {
+            staff.ForEach(Console.WriteLine);
+        }
+
+        /// <summary>
+        /// Returns the Employee with the given ID, or null if no such employee exists.
+        /// </summary>
+        public Researcher Use(int id)
+        {
+            foreach (Researcher e in staff)
+            {
+                if (e.ID == id)
+                {
+                    return e;
+                }
+            }
+            return null;
+            //FYI, if you have an interest in lambda expressions the above could be achieved with:
+            //return staff.First(e => e.ID == id);
+        }
+
+        //This version of Filter modifies the viewable list instead of returning a new list,
+        //but the procedure is almost the same
+        public void Filter(Gender gender)
+        {
+            var selected = from Researcher e in staff
+                           where gender == Gender.Any || e.Gender == gender
+                           select e;
+            viewableStaff.Clear();            
+            //Converts the result of the LINQ expression to a List and then calls viewableStaff.Add with each element of that list in turn
+            selected.ToList().ForEach( viewableStaff.Add );
+        }
+   
+    }
+}
---
+++ Researcher.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_A2      //used to be KIT206_Week9
+{
+
+    //As an example, this includes an additional 'gender' called Any that could be used in a GUI drop-down list.
+    //The filtering could then be modified that if Gender.Any is selected that the full list is returned with no filtering performed.
+    public enum Gender { Any, M, F, X };
+
+    /// <summary>
+    /// Researchers employed by the university
+    /// </summary>
+    public class Researcher
+    {
+        public int ID { get; set; } //need to update these too, looking at the Research Assessment Program document
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public List<Publication> Skills { get; set; } //change variable name (skills --> ?)
+        public string ResearcherType { get; set; }  //can either be staff or student
+
+        public int SkillCount   //change variable name here too
+        {
+            get { return Skills == null ? 0 : Skills.Count(); }
+        }
+
+        //The SkillCount out of 10, expressed as a percentage
+        public double SkillPercent      //is this one needed? might be skill count average over three years?
+        {
+            //This is equivalent to SkillCount / 10.0 * 100
+            get { return SkillCount * 10.0; }
+        }
+
+        //This is likely the solution you will have devised
+        public DateTime MostRecentTraining      //need to change name
+        {
+            get     //could change skillDates too?
+            {
+                var skillDates = from Publication s in Skills
+                                 orderby s.AvailabilityDate descending
+                                 select s.AvailabilityDate;
+                return skillDates.First();
+            }
+        }
+
+        //This is a more robust implementation, but requires the the return type be made 'nullable'
+        //        public DateTime? MostRecentTraining
+        //        {
+        //            get
+        //            {
+        //                if (SkillCount > 0)
+        //                {
+        //                    var skillDates = from TrainingSession s in Skills
+        //                                     orderby s.Certified descending
+        //                                     select s.Certified;
+        //                    return skillDates.First();
+        //                }
+        //                return null;
+        //            }
+        //        }
+
+        //May or may not be useful, but adding it here just in case (DELETE IF OBSOLETE)
+        public int RecentPublication()
+        {
+            if (Skills != null)
+            {
+                int endYear = DateTime.Today.Year;
+                int startYear = endYear - 1; //window is up to 2 years in length
+                var allRecent = from Publication skill in Skills
+                                where skill.PublicationYear >= startYear && skill.PublicationYear <= endYear
+                                select skill;
+                return allRecent.Count();
+
+                //which could be rewritten as a single expression:
+                //return (from Publication skill in Skills
+                //        where skill.Year >= startYear && skill.Year <= endYear
+                //        select skill).Count();
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            //For the purposes of this week's demonstration this returns only the name
+            return Name;
+        }
+    }
+}
